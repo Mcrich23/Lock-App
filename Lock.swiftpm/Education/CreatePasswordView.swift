55129abdf6f0350890480da52cc060ad8fd9654f
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2024-11-17 21:28:06 -0800

    add password meter view

---
+++ Lock.swiftpm/Education/CreatePasswordView.swift
@@ -0,0 +1,125 @@
+//
+//  CreatePasswordView.swift
+//  Pass
+//
+//  Created by Morris Richman on 11/17/24.
+//
+
+import SwiftUI
+
+struct CreatePasswordView: View {
+    @State var password = ""
+    
+    private var requirements: Array<Bool> {
+      [
+        password.count >= 7, // Minimum Length
+        password.contains(where: { $0.isNumber }), // One Number
+        password.range(of: "[ !\"#$%&'()*+,-./:;<=>?@\\[\\\\\\]^_`{|}~]+", options: .regularExpression) != nil // One Special Character
+      ]
+    }
+    
+    private var requirementsBarValue: Double {
+        requirements.reduce(0) { $0 + ($1 ? 1 : 0) }
+    }
+    
+    private var requirementsBarText: String {
+        switch requirementsBarValue {
+        case 0, 1:
+            "Weak"
+        case 2:
+            "Ok"
+        case 3:
+            "Strong"
+        default:
+            "Unkown"
+        }
+    }
+    
+    private var requirementsBarColor: Color {
+        switch requirementsBarValue {
+        case 0, 1:
+            Color.red
+        case 2:
+            Color.yellow
+        case 3:
+            Color.green
+        default:
+            Color.red
+        }
+    }
+    
+    private var topIcon: String {
+        guard !password.isEmpty else {
+            return "custom.lock.badge.plus"
+        }
+        
+        switch requirementsBarValue {
+        case 0, 1:
+            return "custom.lock.badge.exclamationmark"
+        case 2:
+            return "custom.lock.trianglebadge.exclamationmark"
+        case 3:
+            return "custom.lock.badge.checkmark"
+        default:
+            return "custom.lock.badge.plus"
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Image(topIcon)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100, height: 100)
+                .symbolRenderingMode(password.isEmpty ? .monochrome : .multicolor)
+                .foregroundStyle(Color.accentColor)
+                .contentTransition(.symbolEffect(.replace.magic(fallback: .replace)))
+            Text("Set a New Password")
+                .font(.title)
+            
+            if !password.isEmpty {
+                HStack {
+                    ProgressView(value: requirementsBarValue, total: Double(requirements.count)) {
+                        //                    Text("Valid")
+                        EmptyView()
+                    }
+                    .frame(maxWidth: 400)
+                    .progressViewStyle(.linear)
+                    .tint(requirementsBarColor)
+                    
+                    Text(requirementsBarText)
+                        .foregroundStyle(requirementsBarColor)
+                }
+            }
+            
+            HStack {
+                TextField("Enter your new password", text: $password.animation(.default))
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .scaleEffect(1.2)
+                    .frame(maxWidth: 350)
+                    .padding(.horizontal)
+                    .textInputAutocapitalization(.never)
+                    .padding(.trailing)
+                
+                Button("Enter", action: {})
+                    .buttonStyle(.borderedProminent)
+                    .disabled(password.isEmpty)
+            }
+        }
+//        .alert("Incorrect Password", isPresented: $isShowingIncorrectPassword) {
+//            Button("OK") {}
+//        } message: {
+//            Text("Check the hint")
+//        }
+    }
+}
+
+#Preview {
+    CreatePasswordView()
+}
+
+extension String {
+    static func &= (lhs: String, rhs: String) -> Bool {
+        return lhs.range(of: rhs,options: .regularExpression) != nil
+    }
+}
--- Lock.swiftpm/Education/EducationNavigationView.swift
+++ Lock.swiftpm/Education/EducationNavigationView.swift
@@ -8,7 +8,49 @@
 import SwiftUI
 
 struct EducationNavigationView: View {
+    enum ShownView: Int {
+        case poorPassword
+        
+        mutating func next() {
+            if let newValue = ShownView(rawValue: self.rawValue + 1) {
+                self = newValue
+            }
+        }
+        
+        mutating func previous() {
+            if let newValue = ShownView(rawValue: self.rawValue - 1) {
+                self = newValue
+            }
+        }
+    }
+    
+    @State var shownView: ShownView = .poorPassword
+    
     var body: some View {
-        PoorPasswordView()
+        switch shownView {
+        case .poorPassword:
+            PoorPasswordView()
+                .fillSpaceAvailable()
+                .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
+        }
+    }
+}
+struct FillAllSpaceModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                content
+                Spacer()
+            }
+            Spacer()
+        }
+    }
+}
+
+extension View {
+    func fillSpaceAvailable() -> some View {
+        modifier(FillAllSpaceModifier())
     }
 }
