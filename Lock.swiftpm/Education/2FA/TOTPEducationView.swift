9db592fcead42181ae492235ff4e3e236ab9427c
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2024-11-20 20:44:50 -0800

    finished totp education view

--- Lock.swiftpm/Education/2FA/TOTPEducationView.swift
+++ Lock.swiftpm/Education/2FA/TOTPEducationView.swift
@@ -13,6 +13,22 @@ struct TOTPEducationView: View {
     @State var qrCode: UIImage?
     @Environment(\.dismiss) var dismiss
     @State var isShowingIncorrectCodeAlert = false
+    @State var qrCodeAnimationBarValue: CGFloat = 0
+    @State var qrCodeAnimationGridValue: CGFloat = 0
+    @State var isShowingQRCodeAnimationMovingDown = false
+    @State var isShowingQRCodeAnimation = false
+    let qrAnimationSpeed = 2.0
+    var qrAnimation: Animation { .easeInOut.speed(qrAnimationSpeed) }
+    
+    var qrCodeSquareStack: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<4) { i in
+                Rectangle()
+                    .fill(Color.clear)
+                    .stroke(Color.accentColor, lineWidth: 4)
+            }
+        }
+    }
     
     var body: some View {
         @Bindable var setup2FAController = setup2FAController
@@ -24,17 +40,51 @@ struct TOTPEducationView: View {
                 .multilineTextAlignment(.center)
             Text("Since the invention of SMS MFA, Time-Based One Time Passcodes (TOTP) have become the most popular method of authentication. It provides a unique code based on a private key and time, often every 30 seconds. This private key is often imported with a QR code.")
             
-            if let qrCode {
-                Image(uiImage: qrCode)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(maxWidth: 400, maxHeight: 400)
-            } else {
-                Image(systemName: "photo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(maxWidth: 400, maxHeight: 400)
+            GeometryReader { geo in
+                VStack {
+                    if let qrCode {
+                        Image(uiImage: qrCode)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    } else {
+                        Image(systemName: "photo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
+                }
+                .overlay {
+                    VStack(spacing: 0) {
+//                        qrCodeSquareStack
+//                            .opacity((qrCodeAnimationGridValue > 0.75 && qrCodeAnimationGridValue <= 1.1) ? 1 : 0)
+//                        qrCodeSquareStack
+//                            .opacity((qrCodeAnimationGridValue > 0.5 && qrCodeAnimationGridValue <= 1.1) ? 1 : (qrCodeAnimationGridValue > 0.75 && qrCodeAnimationGridValue <= 1) ? 0.5 : 0)
+//                            .opacity((qrCodeAnimationGridValue > 0.25 && qrCodeAnimationGridValue <= 1.1) ? 1 : (qrCodeAnimationGridValue > 0.5 && qrCodeAnimationGridValue <= 0.75) ? 0.5 : 0)
+//                        qrCodeSquareStack
+//                            .opacity((qrCodeAnimationGridValue > 0 && qrCodeAnimationGridValue <= 1.1) ? 1 : (qrCodeAnimationGridValue > 0.25 && qrCodeAnimationGridValue <= 0.5) ? 0.5 : 0)
+                        ForEach(Array(1...4), id: \.self) { i in
+                            HStack(spacing: 0) {
+                                ForEach(0..<4) { i in
+                                    Rectangle()
+                                        .fill(Color.clear)
+                                        .stroke(Color.accentColor, lineWidth: 4)
+                                }
+                            }
+                            .opacity(qrCodeAnimationGridValue == (Double(i)*0.25) ? 1 : (qrCodeAnimationGridValue >= (Double(i)*0.25) && qrCodeAnimationGridValue <= (Double(i+1)*0.28)) ? 0.5 : 0)
+                        }
+                    }
+                    .frame(width: geo.size.width, height: geo.size.height)
+                    .rotation3DEffect(.degrees(isShowingQRCodeAnimationMovingDown ? 0 : 180), axis: (x: 0, y: 0, z: 1))
+                }
+                .overlay {
+                    Capsule()
+                        .fill(Color.accentColor)
+                        .frame(width: geo.size.width+50, height: 15)
+                        .offset(y: (geo.size.height/2) - geo.size.height*qrCodeAnimationBarValue)
+                        .opacity(isShowingQRCodeAnimation ? 1 : 0)
+                        .rotation3DEffect(.degrees(isShowingQRCodeAnimationMovingDown ? 180 : 0), axis: (x: 0, y: 0, z: 1))
+                }
             }
+            .frame(maxWidth: 400, maxHeight: 400)
             
             ZStack(alignment: .top) {
                 Button("Scan QR Code", action: showCode)
@@ -73,7 +123,8 @@ struct TOTPEducationView: View {
                     }
                     .frame(maxWidth: setup2FAController.completedTotp ? 400 : 350)
                 }
-                .offset(y: setup2FAController.isShowingOtp ? 0 : 250)
+                .offset(y: setup2FAController.isShowingOtp ? 0 : -300)
+                .opacity(setup2FAController.isShowingOtp ? 1 : 0)
                 .allowsHitTesting(setup2FAController.isShowingOtp)
             }
         }
@@ -92,9 +143,71 @@ struct TOTPEducationView: View {
         }
     }
     
-    func showCode() {
-        withAnimation {
-            setup2FAController.isShowingOtp = true
+    private func scanQRCodeUp() async {
+        withAnimation(.easeInOut.speed(qrAnimationSpeed/8)) {
+            qrCodeAnimationBarValue = 1
+        }
+        
+        try? await Task.sleep(for: .milliseconds(200/qrAnimationSpeed))
+        
+        await withCheckedContinuation { continuation in
+            withAnimation(qrAnimation) {
+                isShowingQRCodeAnimation = true
+            } completion: {
+                withAnimation(.easeInOut.speed(qrAnimationSpeed/8)) {
+                    qrCodeAnimationBarValue = 1.05
+                }
+                withAnimation(qrAnimation) {
+                    qrCodeAnimationGridValue = 0.25
+                } completion: {
+                    withAnimation(qrAnimation) {
+                        qrCodeAnimationGridValue = 0.5
+                    } completion: {
+                        withAnimation(qrAnimation) {
+                            qrCodeAnimationGridValue = 0.75
+                        } completion: {
+                            withAnimation(qrAnimation) {
+                                qrCodeAnimationGridValue = 1.0
+                            } completion: {
+                                withAnimation(qrAnimation) {
+                                    qrCodeAnimationGridValue = 1.2
+                                } completion: {
+                                    withAnimation(qrAnimation) {
+                                        qrCodeAnimationGridValue = 1.5
+                                    } completion: {
+                                        continuation.resume()
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private func showCode() {
+        Task {
+            self.isShowingQRCodeAnimationMovingDown = false
+            qrCodeAnimationBarValue = 0
+            await scanQRCodeUp()
+            try? await Task.sleep(for: .milliseconds(750/qrAnimationSpeed))
+            self.isShowingQRCodeAnimationMovingDown = true
+            self.qrCodeAnimationGridValue = 0
+            self.qrCodeAnimationBarValue = -0.05
+            withAnimation(.easeInOut.speed(qrAnimationSpeed/7.5)) {
+                qrCodeAnimationBarValue = 0.75
+            }
+//            try? await Task.sleep(for: .milliseconds(45/qrAnimationSpeed))
+            Task {
+                try? await Task.sleep(for: .milliseconds(850/qrAnimationSpeed))
+                withAnimation(.easeInOut.speed(0.7)) {
+                    setup2FAController.isShowingOtp = true
+                    qrCodeAnimationGridValue = 0
+                    isShowingQRCodeAnimation = false
+                }
+            }
+            await scanQRCodeUp()
         }
     }
     
