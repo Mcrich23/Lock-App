ff8de8443ecdb72cb130adab358b6d59871d166c
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2024-12-02 22:46:14 -0800

    added password masking ui

--- Lock.swiftpm/Password Manager/PasswordItemDetailView.swift
+++ Lock.swiftpm/Password Manager/PasswordItemDetailView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct PasswordItemDetailView: View {
     @Bindable var item: Item
     @Environment(AES.self) var aes
-    @Environment(\.modelContext) var modelContext
+    @Environment(\.colorScheme) var colorScheme
     @Namespace var namespace
     
     @State var isEditing: Bool = false
+    @State var isShowingPassword: Bool = false
     @State var textPassword: String? = nil
     
     var body: some View {
@@ -48,10 +49,7 @@ struct PasswordItemDetailView: View {
                     }
                     
                     editableRowItem(withName: "User Name", binding: $item.userName)
-                    editableRowItem(withName: "Password", binding: $textPassword)
-                        .onChange(of: textPassword) {
-                            item.setPassword(textPassword, using: aes)
-                        }
+                    passwordRow
                     editableRowItem(withName: "Website", binding: $item.website, isShowing: item.name?.isEmpty == false)
                 }
             }
@@ -108,6 +106,66 @@ struct PasswordItemDetailView: View {
                     .multilineTextAlignment(.trailing)
                     .textFieldStyle(.emptyable(with: $item.userName))
                     .matchedGeometryEffect(id: "\(name)_text", in: namespace)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    var passwordRow: some View {
+        if let text = textPassword, !text.isEmpty, !isEditing {
+            Divider()
+            
+            HStack {
+                Text("Password")
+                
+                Spacer()
+                
+//                Text(isShowingPassword ? text : "**************")
+                Text(text)
+                    .matchedGeometryEffect(id: "password_text", in: namespace)
+                    .onTapGesture {
+                        isShowingPassword.toggle()
+                    }
+                    .background(content: {
+                        RoundedRectangle(cornerRadius: 4)
+                            .fill(!isShowingPassword ? .clear : Color(uiColor: .tertiarySystemBackground))
+                            .opacity(!isShowingPassword ? 0 : 1)
+                            .onTapGesture {
+                                isShowingPassword.toggle()
+                            }
+                            .scaleEffect(x: 1.2)
+                    })
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 4)
+                            .stroke(isShowingPassword ? .primary : Color(uiColor: .secondarySystemBackground), lineWidth: 2)
+                            .shadow(color: .primary, radius: 2)
+                            .overlay(content: {
+                                RoundedRectangle(cornerRadius: 6)
+                                    .fill(isShowingPassword ? .clear : Color(uiColor: .secondarySystemBackground))
+                                    .onTapGesture {
+                                        isShowingPassword.toggle()
+                                    }
+                            })
+                            .opacity(isShowingPassword ? 0 : 1)
+                            .scaleEffect(x: 1.2)
+                    }
+                    .animation(.default, value: isShowingPassword)
+            }
+        } else if isEditing {
+            Divider()
+            
+            HStack {
+                Text("Password")
+                
+                Spacer()
+                
+                TextField("Password", text: optionalToRequiredStringBinding($textPassword))
+                    .multilineTextAlignment(.trailing)
+                    .textFieldStyle(.emptyable(with: $item.userName))
+                    .matchedGeometryEffect(id: "password_text", in: namespace)
+                    .onChange(of: textPassword) {
+                        item.setPassword(textPassword, using: aes)
+                    }
             }
         }
     }
